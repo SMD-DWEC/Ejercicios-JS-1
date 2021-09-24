ddb5977a065fc9a1eb1bcc88ef7130c802ac39f0
usasergio <usasergio2@gmail.com>
2021-09-24 17:53:02 +0200

    Arreglado Array de palabras

--- js/script.js
+++ js/script.js
@@ -2,38 +2,32 @@
 //Fecha: 24-9-2021
 
 //Enunciado: Escribe un programa que pida una frase y escriba las vocales que aparecen
-
-let input = undefined;
-let vocales = undefined;
-
-input = prompt("Escriba una frase");
+let input = prompt("Escriba una frase");
 
 
 input = input.toLocaleLowerCase();
 
 document.write('Vocales: ');
 
-//Array de palabras:
-let palabrasPermitidas = [
-    ['a','e','i','o','u'],
-    ['á','é','í','ó','ú'],
-    ['ä','ë','ï','ö','ü'],
-    ['â','ê','î','ô','û']
-]
 
-for(i=0;i<=input.length;i++) {
-    if(allowed(input.charAt(i) )){ //input.charAt(i) == palabrasPermitidas 
-        console.log(input[i]);
-        document.write(`${input[i]}`);
+for(let i=0;i<input.length;i++) {
+    if(allowed(input.charAt(i) )){
+        document.write(`${input[i]}, `);
     }
 }
 
-
+//Comprueba que la palabra esté en la lista de admitidas, entonces
+//devuelve true.
 function allowed(palabra) {
-    for(i=0;i<=palabrasPermitidas.length;i++) {
-        console.log(palabrasPermitidas[i])
+    let palabrasPermitidas = [
+        'a','e','i','o','u',
+        'á','é','í','ó','ú',
+        'ä','ë','ï','ö','ü',
+        'â','ê','î','ô','û'
+    ];
+
+    for(let i=0;i<palabrasPermitidas.length;i++) {
         if(palabra == palabrasPermitidas[i]) {
-            console.log("entra")
             return true;
         }
     }
